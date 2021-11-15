011566117ee71cdb22aacf1d006e16ce95d87a4f
Samuel Kong <kongsamuel1999@gmail.com>
2021-11-15 12:23:16 -0500

    Changed to access env port

--- server.js
+++ server.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const nano = require('nano')('http://admin:password@localhost:5984');
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -14,5 +13,5 @@ app.use(express.json())
 app.use('/users', usersRouter);
 
 
-app.listen(PORT, HOST)
-console.log('Running on ${HOST}:${PORT}');
+app.listen(PORT)
+console.log('Running on ${PORT}');
