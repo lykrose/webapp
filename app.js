6dc795b06e9216d3c1be0628a5b8fd62f50024ef
Samuel Kong <kongsamuel1999@gmail.com>
2021-12-29 15:30:04 -0500

    added additional routs

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const nano = require('nano')('http://admin:password@localhost:5984');
 const app = express();
 
 var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var aboutRouter = require('./routes/about');
 
 app.use(express.json())
 
 app.use('/users', usersRouter);
+app.use('/', homeRouter);
+app.use('/about', aboutRouter);
 
 module.exports = app;
