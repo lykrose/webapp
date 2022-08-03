e3c13d07805fbb71d8e5d2509a2a7bdb983f78ab
Samuel Kong <kongsamuel1999@gmail.com>
2022-08-03 00:42:14 -0400

    Fixed database start up

--- backend/couchdb/db_init.js
+++ backend/couchdb/db_init.js
@@ -1,21 +1,16 @@
 var nano = require('nano')('http://admin:password@localhost:5984');
 
-var databases = ['users'];
+nano.db.create("_users")
 
-databases.forEach(function(entry) {
-    console.log(entry);
-
-    // create a new database if it does not exist already
-    nano.db.create(entry, (err, data) => {
-        if (err) {
-            console.log('Error:',err.message);
-        } else {
-            var new_db = nano.use(entry)
-            var data = require("./data/" + entry + ".json");
-            new_db.bulk(data).then((body) => {
-                console.log(body);
-            });
-        };
-    });
+// create a new database if it does not exist already
+nano.db.create("users", (err, data) => {
+    if (err) {
+        console.log('Error:',err.message);
+    } else {
+        var new_db = nano.use("users")
+        var data = require("./data/users.json");
+        new_db.bulk(data).then((body) => {
+            console.log(body);
+        });
+    };
 });
-
